fac9fce28707587e4848e4207a60ec53010f1b61
amirkakon <amir.kakon@gmail.com>
2025-08-21 12:30:02 +0300

    route fixes

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,9 @@ const { app, functions } = require("./setup");
 
 // app routes
 require("./Routes/Controllers/Auth");
-require("./Routes/Controllers/Coupons");
+require("./Routes/Controllers/Forms");
+require("./Routes/Controllers/Tasks");
+require("./Routes/Controllers/Summary");
 
 // Export the main app
 exports.app = functions.https.onRequest(app);
